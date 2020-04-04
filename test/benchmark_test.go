bb8c62b491632520c17b30aeae7378ae68234db2
Parham Alvani <parham.alvani@gmail.com>
2020-04-04 16:43:54 +0000

    Run Clients in Benchmark Test

--- test/benchmark_test.go
+++ test/benchmark_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/elahe-dastan/applifier/config"
+	"github.com/elahe-dastan/applifier/internal/client"
 	"github.com/elahe-dastan/applifier/internal/server"
 	"github.com/stretchr/testify/require"
 )
@@ -13,6 +14,17 @@ const benchmarkServerPort = "8080"
 
 func TestBenchmark(t *testing.T) {
 	srv := server.New()
-	cfg := config.ServerConfig{Address: ":" + benchmarkServerPort}
-	require.NoError(t, srv.Start(cfg))
+	cfg := config.Config{Address: ":" + benchmarkServerPort}
+
+	go func() {
+		require.NoError(t, srv.Start(cfg))
+	}()
+
+	for i := 0; i < clientCount; i++ {
+		cli := client.New()
+
+		go func() {
+			require.NoError(t, cli.Connect("127.0.0.1:"+benchmarkServerPort))
+		}()
+	}
 }
